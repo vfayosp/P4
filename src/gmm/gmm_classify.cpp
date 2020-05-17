213d0c638e8b463db7a56e02a47e3b16ec72286a
Victor Fayos PAV <victor.fayos@estudiant.upc.edu>
2020-05-17 02:28:10 +0200

    classerr

--- src/gmm/gmm_classify.cpp
+++ src/gmm/gmm_classify.cpp
@@ -29,8 +29,10 @@ int classify(const vector<GMM> &vgmm, const fmatrix &dat, float &maxlprob) {
   //for each gmm, call logprob. Implement this function in gmm.cpp
   maxind = 0;
   for (unsigned int i = 0; i < vgmm.size(); i++){
-    if (vgmm[i].logprob(dat) > maxlprob){
+    lprob=vgmm[i].logprob(dat);
+    if (lprob > maxlprob){
       maxind = i;
+      maxlprob = lprob;
     }
   }
   return maxind;
